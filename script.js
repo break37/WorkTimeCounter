745c8c6e09a2b2eee66aeb79f0270f72fcf9b5ea
Hubert Wawrzacz <hwawrzac@wasko.pl>
2019-08-14 08:15:15 +0200

    Fix displaying 0% progress when refreshed after 100% has been reached

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
-	#region Variables declarations
+	// #region Variables declarations
 	var startDate;
 	var startH;
 	var startM;
-	var startSecs 
-	var finishSecs
+	var startSecs;
+	var finishSecs;
 	var startTime;
 	var finishTime;
 	var currDate;
 	var currentSecs;
-	#endregion
+	// #endregion
 	
 	// uncomment below if you want to start couting automatically on page load
 	// be careful however, because countdown will start from the beginning every time page is reloaded
-	// startDate = new Date();
+	// startTime = new Date();
 	
 	// or set time at which you start your work
 	// format is (YYYY, MM-1, DD, HH, MM, SS)
-	startDate = new Date(2019, 07, 13, 07, 25, 00);
+	startDate = new Date(2019, 07, 14, 07, 05, 00);
 	
 	calculateStartAndfinishTime();		
-	showStartAndFinishtime()
+	showStartAndFinishTime();
 	
 	refreshTime();
 	startInterval();
 	
-	#region Functions
+	// #region Functions
 	function calculateStartAndfinishTime(){
 		startH = startDate.getHours();
 		startM = startDate.getMinutes();
@@ -38,7 +38,7 @@ $(document).ready(function(){
 		finishSecs = startSecs + parseInt(8*60*60*1000);
 	}	
 	
-	showStartAndFinishtime(){
+	function showStartAndFinishTime(){
 		$("#startTime").html("Godzina rozpoczęcia: " + startTime);
 		$("#finishTime").html("Godzina zakończenia: " + finishTime);
 	}
@@ -58,13 +58,18 @@ $(document).ready(function(){
 		currDate = new Date();
 		currentSecs = currDate.getTime();
 		var millisecsToHoursMultiplier = 60*60*1000;
+		var progress = Math.round(100 - parseFloat(finishSecs-currentSecs)/millisecsToHoursMultiplier/8*100);
 		
-		if (currentSecs <= finishSecs){	
-			var progress = Math.round(100 - parseFloat(finishSecs-currentSecs)/millisecsToHoursMultiplier/8*100);
+		if (progress < 100){	
 			
 			$("#caption").html("Dzień pracy ukończono w " + progress + "%");
 			$("#progressbar-thumb").css("width", progress + "%");
 		}
+		else if (progress >= 100){	
+			$("#caption").html("Dzień pracy ukończono w " + progress + "%");
+			$("#progressbar-thumb").css("width", progress + "%");
+			$(window).focus();
+		}
 	}
-	#endregion
+	// #endregion
 });
